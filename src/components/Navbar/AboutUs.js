120a269035e8b2bacbd3726b17d2c791804963df
shamikhquraan1 <shamikhq@yahoo.com>
2021-09-02 07:12:06 +0300

    adding final touchs

--- src/components/Navbar/AboutUs.js
+++ src/components/Navbar/AboutUs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './AboutUs.css'
+import './AboutUs.css';
 class AboutUs extends Component {
   render() {
     return (
@@ -9,7 +9,6 @@ class AboutUs extends Component {
             <div className="content">
               <h2> Malik Swayyed </h2>
               <p>Mechanical engineer has start a new path in programming, it was a hoppy but not any more :)</p>
-              <a href="https://github.com/mechengmalik" target="_blank"> Github</a>
             </div>
             <img src="https://avatars.githubusercontent.com/u/85099893?v=4" alt="Malik Swayyed" />
           </div>
@@ -17,37 +16,35 @@ class AboutUs extends Component {
             <div className="content">
               <h2>Abdalrahman Abualata</h2>
               <p>I'm 26 years old ,I'm a civil engineering , I have a little bit knowledge in java and I'm looking forward to learn more.</p>
-              <a href="https://github.com/AbdalrahmanAbualata" target="_blank"> Github</a>
             </div>
             <img src="https://avatars.githubusercontent.com/u/85103825?v=4" alt="Abdalrahman Abualata" />
           </div>
           <div className="cards">
             <div className="content">
+              <h2>Shamikh quraan</h2>
+              <p> Hey! im Shamikh Power Engineer, programmer, Designer ,unshakable
+                optimist dedicated to be the best version of me ,Collaborative
+                at my zone and for sure Helpful.</p>
+            </div>
+            <img src="https://avatars.githubusercontent.com/u/85109430?v=4" alt="Shamikh quraan" />
+          </div>
+          <div className="cards">
+            <div className="content">
               <h2>Bashar Taamneh</h2>
               <p> I'm 26 years old I obtained a Bachelor’s degree from Al Hussein Bin Talal University in computer engineering.</p>
-              <a href="https://github.com/BasharTaamneh" target="_blank"> Github</a>
             </div>
             <img src="https://avatars.githubusercontent.com/u/85108031?v=4" alt="Bashar Taamneh" />
           </div>
           <div className="cards">
             <div className="content">
               <h2>Abdullah Nazzal</h2>
-              <p>I'm 25 years old, I'm graduated from the Jadara University, majored Software Engineering.</p>
-              <a href="https://github.com/abdullahnazzal" target="_blank"> Github</a>
+              <p>I'm 25 years old, I'm graduated from the Jadara University, majoressd Software Engineering.</p>
             </div>
             <img src="https://avatars.githubusercontent.com/u/52371286?v=4" alt="Abdullah Nazzal" />
           </div>
-          <div className="cards">
-            <div className="content">
-              <h2>Shamikh quraan</h2>
-              <p> I'm electical engineer and nearly inshalla will devlopar, i was gratuated from
-                yarmouk unvirsety.</p>
-              <a href="https://github.com/Shamikhquraan" target="_blank"> Github</a>
-            </div>
-            <img src="https://avatars.githubusercontent.com/u/85109430?v=4" alt="Shamikh quraan" />
-          </div>
+       
         </div>
-      </>
+        </>
     );
   }
 }
--- src/components/Navbar/Profile.js
+++ src/components/Navbar/Profile.js
@@ -1,17 +1,69 @@
-import React, { Component } from 'react';
+import React from "react";
+import'./Profile.css'
+
+// reactstrap components
+import { Card, Container, Row, Col } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { withAuth0 } from '@auth0/auth0-react';
-class Profile extends Component {
+
+// core components
+
+class Profile extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
   render() {
-    const { user, isAuthenticated  } = this.props.auth0;
     return (
-        isAuthenticated && (
-          <div>
-            <img id="" src={user.picture} alt={user.name} />
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </div>
-        )
-      );
+      <>
+        <main className="profile-page" ref="main">
+         
+          <section className="section">
+            <Container>
+              <Card className="card-profile">
+                <div className="px-4">
+                  <Row className="justify-content-center">
+                    <Col className="order-lg-3" lg="3">
+                      <div className="card-profile-image">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                          <img
+                            alt="..."
+                            className="rounded-circle"
+                            width="50%"
+                            src={this.props.auth0.user.picture}
+                          />
+                        </a>
+                      </div>
+                    </Col>
+                    <Col
+                      className="order-lg-3 text-lg-right align-self-lg-center"
+                      lg="4"
+                    >
+                    </Col>
+                  </Row>
+                  <div className="text-center mt-5">
+                    <h3>
+                      {this.props.auth0.user.name}{" "}
+                      <span className="font-weight-light">, 25</span>
+                    </h3>
+                    <div className="h6 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Jordan, Irbed
+                    </div>
+                    <div className="h6 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      {this.props.auth0.user.email}
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </Container>
+          </section>
+        </main>
+      </>
+    );
   }
 }
+
 export default withAuth0(Profile);
